fa80d0296acad1ce9151aea7aa7fd592369547bb
Golam <golam.rabbi@klovercloud.com>
2022-02-08 18:03:42 +0600

    auth, shared module and login, registration component added with lazy loading approch

---
+++ src/app/auth/auth-routing.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import {RouterModule, Routes} from "@angular/router";
+import {LoginComponent} from "./login/login.component";
+
+const routes: Routes = [
+  {
+    path: 'login',
+    component: LoginComponent,
+    data: {
+      title: 'CI-console | Log In'
+    }
+  },
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full',
+  },
+  { path: '**',
+    redirectTo: 'login',
+    pathMatch: 'full'
+  }
+]
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class AuthRoutingModule { }
