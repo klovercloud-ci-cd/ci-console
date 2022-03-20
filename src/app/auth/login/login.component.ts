00b83acfb876cb5fc49bd2f7edf5f80f820a18b5
kabir-webDev <kabir.webdev@gmail.com>
2022-03-20 17:57:05 +0600

    Loader Added

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../auth.service';
 import { TokenService } from '../token.service';
-import { FormBuilder, Validators} from "@angular/forms";
-import {Router} from "@angular/router";
+import { FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -29,7 +29,6 @@ export class LoginComponent implements OnInit {
     password: ['', Validators.required],
   });
   logIn(): void {
-<<<<<<< HEAD
     this.authService
       .login({
         email: this.loginForm.value.email,
@@ -38,16 +37,9 @@ export class LoginComponent implements OnInit {
       .subscribe((res) => {
         this.tokenService.saveAccessToken(res.data.access_token);
         this.tokenService.saveRefreshToken(res.data.refresh_token);
+        this.router.navigate(['']);
         console.log(this.authService.getUserData(), 'USER');
       });
-=======
-    this.authService.login({email: this.loginForm.value.email, password: this.loginForm.value.password}).subscribe(res => {
-      this.tokenService.saveAccessToken(res.data.access_token);
-      this.tokenService.saveRefreshToken(res.data.refresh_token);
-      this.router.navigate([''])
-      console.log(this.authService.getUserData(), 'USER')
-    })
->>>>>>> 0311899e315082322b8fa29acdef95315a40b50a
   }
   logout(): void {
     this.authService.logOut();
