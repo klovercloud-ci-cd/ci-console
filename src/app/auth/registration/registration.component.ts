e558ef923ffdd39f5d736dd032654ea26fd432c2
kabir-webDev <kabir.webdev@gmail.com>
2022-03-20 17:46:47 +0600

    Loader Added

--- src/app/auth/registration/registration.component.ts
+++ src/app/auth/registration/registration.component.ts
@@ -12,18 +12,20 @@ import { AuthService } from '../auth.service';
 export class RegistrationComponent implements OnInit {
   registrationForm = this.fb.group(
     {
-      firstname: ['', [Validators.required, Validators.maxLength(15)]],
-      lastname: ['', [Validators.required, Validators.maxLength(15)]],
+      first_name: ['', [Validators.required, Validators.maxLength(15)]],
+      last_name: ['', [Validators.required, Validators.maxLength(15)]],
       email: ['', [Validators.required, Validators.email]],
       phone: ['', [Validators.required]],
-      password: ['', Validators.required],
-      checked: [false, Validators.requiredTrue],
+      password: ['', [Validators.required, Validators.minLength(8)]],
       c_password: ['', Validators.required],
+      checked: [false, Validators.requiredTrue],
+      auth_type: ['password'],
     },
     {
       validator: this.confirmPasswordMatch('password', 'c_password'),
     }
   );
+  isLoading = false;
 
   constructor(
     private fb: FormBuilder,
@@ -33,40 +35,25 @@ export class RegistrationComponent implements OnInit {
   ) {}
 
   registrationFormData() {
-    console.log('Form Value:', this.registrationForm.value);
+    this.isLoading = true;
+    const formData = this.registrationForm.value;
+    delete formData['c_password'];
+    delete formData['checked'];
+    formData['phone'] = formData.phone.toString();
 
-    this.authService
-      .signUp({
-        first_name: this.registrationForm.value.firstname,
-        last_name: this.registrationForm.value.lastname,
-        email: this.registrationForm.value.email,
-        phone: this.registrationForm.value.phone,
-        password: this.registrationForm.value.password,
-        auth_type: 'password',
-      })
-      .subscribe((res) => {
+    this.authService.signUp(formData).subscribe(
+      (res) => {
+        if (res.status === 'success') {
+          this.isLoading = false;
+          this.router.navigate(['/auth/login']);
+        }
+        console.log(res.status);
         console.log(this.authService.getUserData(), 'USER');
-      });
-
-    // var formData: any = new FormData();
-    // formData.append('first_name', this.registrationForm.value.firstname);
-    // formData.append('last_name', this.registrationForm.value.lastname);
-    // formData.append('email', this.registrationForm.value.email);
-    // formData.append('phone', this.registrationForm.value.phone);
-    // formData.append('password', this.registrationForm.value.password);
-    // formData.append('auth_type', 'password');
-    // console.log('FOrmData: ', formData);
-
-    // this.http
-    //   .post('http://192.168.68.162:4200/api/v1/users', formData)
-    //   .subscribe({
-    //     next: (response) => console.log('This is response: ', response),
-    //     error: (error) => console.log(error),
-    //   });
-
-    // setTimeout(() => {
-    //   this.router.navigate(['/auth/login']);
-    // }, 1000);
+      },
+      (err) => {
+        console.log('err', err);
+      }
+    );
   }
 
   ngOnInit(): void {}
