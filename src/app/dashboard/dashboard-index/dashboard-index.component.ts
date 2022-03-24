166027b6e1f6a84b10784ee1edda7729d9d94b27
ishafiul <shafiulislam20@gmail.com>
2022-03-24 09:39:55 +0600

    add company modal, authguard, and refresh token with interval

---
+++ src/app/dashboard/dashboard-index/dashboard-index.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'kcci-dashboard-index',
+  templateUrl: './dashboard-index.component.html',
+  styleUrls: ['./dashboard-index.component.scss']
+})
+export class DashboardIndexComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
