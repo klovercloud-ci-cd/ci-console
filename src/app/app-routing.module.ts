81403f26e09a62ffd5ca435f2a35240c9e67bca8
ishafiul <shafiulislam20@gmail.com>
2022-04-21 11:11:54 +0600

    svg scroll test for pipe line

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -62,7 +62,7 @@ const routes: Routes = [
     path: '',
     component: LayoutComponent,
     children: childrenRoutes,
-    canActivate: [AuthGuard],
+    /*canActivate: [AuthGuard],*/
   },
   {
     path: '**',
