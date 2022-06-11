a8f3e56304306fd88282ba6a999d46fb0c57b615
kabir-webDev <kabir.webdev@gmail.com>
2022-06-11 14:54:54 +0600

    Sidebar and registration page re designed

--- src/app/core/layout/collapsed-sidebar/collapsed-sidebar.component.ts
+++ src/app/core/layout/collapsed-sidebar/collapsed-sidebar.component.ts
@@ -1,6 +1,9 @@
-import  { OnInit} from '@angular/core';
+import {OnInit, ViewChild} from '@angular/core';
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import  { SharedLayoutService } from '../shared-layout.service';
+import {NavItem, NavItemNode} from "../sidebar/Sidenav";
+import {FlatTreeControl} from "@angular/cdk/tree";
+import {MatTreeFlatDataSource, MatTreeFlattener} from "@angular/material/tree";
 
 @Component({
   selector: 'kcci-collapsed-sidebar',
@@ -9,7 +12,9 @@ import  { SharedLayoutService } from '../shared-layout.service';
 })
 export class CollapsedSidebarComponent implements OnInit {
   openCollapsedBar = false;
+  @ViewChild('tree') tree: any;
 
+  navItems: NavItem[] = [];
   menuItem: any = [
     {
       title: 'Dashboard',
@@ -17,32 +22,49 @@ export class CollapsedSidebarComponent implements OnInit {
       route: '/',
     },
     {
-      title: 'Applications',
-      icon: 'apps',
-      route: '/app/list',
-    },
-    {
       title: 'Repository',
       icon: 'widgets',
       route: '/repository',
-    },
-    {
-      title: 'Pipeline',
-      icon: 'route',
-      route: '/app/pipe',
-    },
-    {
-      title: 'Attach Company',
-      icon: 'store',
-      route: '/attach-company',
     },
   ];
 
   constructor(private ss: SharedLayoutService) {}
 
+  private _transformer = (node: NavItem, level: number) => ({
+    expandable: !!node.children && node.children.length > 0,
+    name: node.name,
+    url: node.url,
+    icon: node.icon,
+    level,
+  })
+
+  // nav
+  treeControl = new FlatTreeControl<NavItemNode>(
+    node => node.level, node => node.expandable);
+
+  treeFlattener: any = new MatTreeFlattener (
+    this._transformer, node => node.level, node => node.expandable, node => node.children);
+
+  dataSource = new MatTreeFlatDataSource(this.treeControl, this.treeFlattener);
+
+  hasChild = (_: number, node: NavItemNode) => node.expandable;
+
   ngOnInit(): void {}
 
   openNav() {
+    this.navItems = [
+      {
+        name: 'Settings',
+        icon: 'settings',
+        isExpanded: true,
+        children: [
+          { name: 'User', icon: 'people', url: '/users' },
+          { name: 'Role', icon: 'supervisor_account', url: '/roles' },
+        ],
+      },
+    ];
+    this.dataSource.data = this.navItems;
+
     setTimeout(() => {
       this.ss.emitData();
     }, 400);
